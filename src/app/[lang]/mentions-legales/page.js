d569b72c6b6099d790338d9c845b14d38bc2e2f4
Liedel <liedel75@gmail.com>
2025-09-29 18:31:17 +0200

    fix build

--- src/app/[lang]/mentions-legales/page.js
+++ src/app/[lang]/mentions-legales/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Layout } from "@/components/Layout"
 import { getSettings, getHeader, getFooter, getLocales } from "@/lib/dataLoader"
 
--- src/app/[lang]/production/page.js
+++ src/app/[lang]/production/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Layout } from "@/components/Layout"
 import ProductionClient from "./ProductionClient"
 import {
